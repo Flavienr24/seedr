83a9e9e2e2f83a67d64ff7e5abdf05c197e1b4df
Flavien Rosellini <rosellini.flavien@gmail.com>
2019-11-26 19:30:57 +0100

    seed file OK. Waiting for the controller and routes to crash test it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+puts 'Creating user'
+bob = User.create!(name: 'Bob', city:'Paris', email:'bob@bob.com', password: 'password')
+puts 'User created'
+
+puts "Let's start the seed for flowers..."
+
+puts "Destroy the database"
+Seedpack.destroy_all
+puts "Destruction done!!!!!!!!!!!!!"
+
+puts "start the seed"
+filepath = "db/json-flowers.json"
+
+serialized_flowers = File.read(filepath)
+
+flowers = JSON.parse(serialized_flowers)
+
+f = flowers.flatten[1]
+
+
+f.each do |seed|
+  Seedpack.create!(
+    name: seed['name'],
+    number: seed['number'].to_i,
+    description: seed['description'],
+    image: seed['image'],
+    price: seed['price'].to_i,
+    user: bob
+  )
+end
+
+puts "seed finished"
