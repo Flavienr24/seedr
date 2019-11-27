eaec8e03805148a861389589f3edf0e33e93e916
Laurent Deguil <ldeguil@yahoo.com>
2019-11-27 14:56:42 +0100

    faker user en attente crash test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake users...'
+10.times do
+  user = User.new(
+    name: Faker::Name,
+    bio: Faker::Lorem,
+    email: Faker::Internet.email,
+    avatar: url="https://source.unsplash.com/featured/?face"
+  )
+  user.save!
+end
+puts 'Finished!'
