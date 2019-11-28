effa2c96c4389b00520070104aaf03ca5c87c7c8
Laurent Deguil <ldeguil@yahoo.com>
2019-11-28 11:29:16 +0100

    maj dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,22 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @disable_nav = true
   end
 
   def dashboard
     # afficher le profile de user ( nom city email avatar)
     @user = current_user
     # afficher l'index des graines de user (image, titre, )
-    @myseeds = Seedpack.where(user: current_user)
+    @myseeds = @user.seedpacks
     # inclure lien pour delete et lien pour modifier chaque graine (fait dans view)
 
     # inclure lien pour poster une graine (fait dans view)
 
     # afficher l'historique des ventes
-    # @mysells = Order.where(seedpack: seedpack(current_user))
-
+    @mysells = Order.where(seedpack_id: @myseeds.ids)
     # afficher l'historique des achats
-    # @mybuys = Order.where( current_user)
+    @mybuys = @user.orders
   end
 end
+
